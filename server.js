8b36d2630b547e18d30c37ce6d6dcd985d2f2590
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2020-07-16 00:32:21 +0530

    Passport authentication added, fixed routes, stable

--- server.js
+++ server.js
@@ -3,141 +3,151 @@ const ejs = require('ejs');
 const quiz = require(__dirname + '/quiz.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({secret: "my secret", resave: false, saveUninitialized: false}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/AspireDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const User = mongoose.model('User', {
-  firstName: String,
-  lastName: String,
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   answers: [Number],
   marks: [mongoose.Decimal128]
 });
 
-app.get('/', function(req, res){
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.get('/', function(req, res) {
+  res.redirect('/sign-in');
+});
+
+app.get('/home', function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render('home');
+  } else {
     res.redirect('/sign-in');
+  }
 });
 
-app.get('/home/:uid', function(req, res){
-    User.findOne({'_id': req.params.uid}, function(err, found){
-        if(err) {
-          res.send('Error');
-        } else if (found) {
-          res.render('home', {user: found});
-        }
-    });
-});
-
-app.get('/sign-up', function(req, res){
+app.get('/sign-up', function(req, res) {
   res.render('sign-up');
 });
 
-app.get('/sign-in', function(req, res){
+app.get('/sign-in', function(req, res) {
   res.render('sign-in');
 });
 
-app.get('/:uid', function(req, res) {
-  const uid = req.params.uid;
+app.get("/sign-out", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
-  User.findOne({
-    '_id': uid
-  }, (err, found) => {
+app.get('/results', function(req, res) {
+  User.findById(req.user._id, function(err, found) {
     if (err) {
       console.log(err);
-    } else if (found) {
-      res.render('result', {marks: found.marks});
+    } else {
+      if (found) {
+        res.render('result', {marks: found.marks});
+      }
     }
   });
 });
 
-app.get('/quiz/:uid', function(req, res) {
-  const uid = req.params.uid;
-  User.findOne({
-    '_id': uid
-  }, (err, found) => {
+app.get('/quiz', function(req, res) {
+  User.findById(req.user._id, function(err, found) {
     if (err) {
-      console.log(err);
-    } else if (found) {
-      res.render('quiz', {
-        user: found,
-        quizTitle: "CPT",
-        questions: quiz.questions,
-        options: quiz.options
-      });
+      console.log("OOPS " + err);
+    } else {
+      if (found) {
+        console.log(found);
+        res.render('quiz', {
+          user: found,
+          quizTitle: "CPT",
+          questions: quiz.questions,
+          options: quiz.options
+        });
+      }
     }
   });
 });
 
 app.post('/sign-up', (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const password = md5(req.body.password);
-  const user = new User({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-    marks: [],
-    answers: []
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/home");
+      });
+    }
   });
-  console.log(user);
-  user.save();
-  res.send('Success!');
 });
 
 app.post('/sign-in', (req, res) => {
-  const email = req.body.email;
-  const password = md5(req.body.password);
-  User.findOne({
-    email: email,
-  }, (e, f) => {
-    if (e) {
-      res.send('Failure!');
+  const user = new User({username: req.body.username, password: req.body.password});
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
     } else {
-      console.log(f);
-      if(f && f.password === password) {
-        console.log('Success!');
-        res.render('home', {user: f});
-      }
-      else {
-        res.send('incorrect username or password');
-      }
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/home");
+      });
     }
   });
 });
 
-app.post('/quiz/:uid', function(req, res) {
-  const uid = req.params.uid;
+app.post('/quiz', function(req, res) {
   const obj = quiz.score(req.body);
-  const marks = obj.marks;
-  const answers = obj.answers;
+  console.log("Marks " + obj.marks);
+  console.log("Answers " + obj.answers);
 
-  console.log("Marks: " + marks);
-  console.log("Answers: " + answers);
-
-  User.updateOne({
-    '_id': uid
-  }, {
-    marks: marks,
-    answers: answers
-  }, function(err, docs) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/" + uid);
-    }
+  User.findByIdAndUpdate(req.user._id, {
+    marks: obj.marks,
+    answers: obj.answers
+  }, function(err, docs){
+      if(err) {
+        console.log(err);
+        res.sendStatus(404);
+      } else {
+        console.log('updated');
+        res.redirect('/results');
+      }
   });
 });
 
